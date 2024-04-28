f778b7b53dda5524280b4941a8fa5dc3ed454d5b
Bernardo Carvalho <beeh.geek@gmail.com>
2024-04-28 09:01:24 -0300

    Atividades Vetores e Matrizes

---
+++ vetoresMatrizes/src/vetoresmatrizes/AtividadeArray1.java
@@ -0,0 +1,12 @@
+package vetoresmatrizes;
+
+public class AtividadeArray1 {
+
+	 public static void main(String[] args) {
+	        String vetorString[] = {"Boxer","Pastor Alemão","Pinscher","Husky Siberiano","Corgi"};
+	        
+	        for(int indice = 0; indice < 5; indice++) {
+	            System.out.println((indice + 1) + "º elemento: " + vetorString[indice]);  
+	        }
+	    }
+	}
